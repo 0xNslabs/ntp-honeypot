8594ddfe181ee63096cc1f826027e0d2f48f15db
iodn <iodn@duck.com>
2024-01-11 12:21:54 +0900

    Add initial NTP honeypot script, example image, and README

---
+++ README.md
@@ -0,0 +1,45 @@
+# Simple NTP Honeypot Server
+
+## Introduction
+The Simple NTP Honeypot Server is a script designed for cybersecurity experts and enthusiasts to study NTP-based network interactions. Crafted in Python using the Twisted framework, this tool simulates an NTP server, effectively logging unauthorized access and interactions. It serves as an essential resource for understanding NTP security vulnerabilities and potential exploitation methods.
+
+## Features
+- **Low-Interaction Honeypot**: Imitates an NTP server, safely capturing and logging interaction data.
+- **Flexible Configuration**: Customizable settings for host and port, adjustable via command-line parameters.
+- **Detailed Interaction Logging**: Documents all NTP requests and responses, offering insights into potential threats.
+- **Real-Time Traffic Monitoring**: Instantaneously logs NTP communication for quick anomaly detection and analysis.
+- **Educational and Research Utility**: Excellent for exploring NTP security and network reconnaissance techniques.
+
+## Requirements
+- Python 3.x
+- Twisted Python library
+
+## Installation
+To install and configure the NTP honeypot server, execute the following steps:
+
+```bash
+git clone https://github.com/0xNslabs/ntp-honeypot.git
+cd ntp-honeypot
+pip install twisted
+```
+
+## Usage
+Launch the server using the following command, with optional arguments for host and port. By default, the server binds to all interfaces (0.0.0.0) on port 123.
+
+```bash
+python3 ntp.py --host 0.0.0.0 --port 123
+```
+
+## Logging
+The server logs all NTP interactions in ntp_honeypot.log, providing detailed accounts of requests and client information.
+
+## Simple NTP Honeypot In Action
+![Simple NTP Honeypot in Action](https://raw.githubusercontent.com/0xNslabs/ntp-honeypot/main/PoC.png)
+*This image demonstrates the Simple NTP Honeypot Server in action, capturing real-time NTP requests and client interactions.*
+
+## Security and Compliance
+- **Caution**: Utilize this honeypot in secure and controlled environments, primarily for educational and research purposes.
+- **Compliance**: Ensure that deployment aligns with local and international legal and ethical guidelines.
+
+## License
+This project is licensed under the MIT License. More details can be found in the LICENSE file.
---
+++ ntp.py
@@ -0,0 +1,76 @@
+import os
+import time
+import argparse
+import struct
+from struct import unpack, pack
+from twisted.internet import reactor
+from twisted.internet.protocol import DatagramProtocol
+from twisted.python import log
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+class SimpleNTPProtocol(DatagramProtocol):
+    NTP_TIMESTAMP_DELTA = 2208988800
+
+    def get_ntp_request_type(self, mode):
+        mode_to_request_type = {
+            0: 'Reserved',
+            1: 'Symmetric Active',
+            2: 'Symmetric Passive',
+            3: 'Client',
+            4: 'Server',
+            5: 'Broadcast',
+            6: 'NTP Control Message',
+            7: 'Private Use'
+        }
+        return mode_to_request_type.get(mode, 'Unknown')
+
+    def system_time_to_ntp(self, time_):
+        return int(time_ + self.NTP_TIMESTAMP_DELTA) << 32
+
+    def datagramReceived(self, data, addr):
+        if len(data) < 48:
+            return
+        try:
+            client_ip, client_port = addr
+            log.msg(f"NTP Request - Client IP: {client_ip}, Port: {client_port}")
+            
+            unpacked = unpack('!B B b b I I 4s Q Q Q Q', data[:48])
+            leap_ver_mode, stratum, poll, precision, \
+            root_delay, root_dispersion, ref_id, \
+            ref_ts, orig_ts, recv_ts, trans_ts = unpacked
+
+            leap = leap_ver_mode >> 6
+            version = leap_ver_mode >> 3 & 0x07
+            mode = leap_ver_mode & 0x07
+            mode = int(mode)
+            version = int(version)
+            request_type = self.get_ntp_request_type(mode)
+
+            log.msg(f"Leap: {leap}, Version: {version}, Mode: {mode}, Request Type: {request_type}, Stratum: {stratum}")
+
+            if mode == 3:
+                recv_timestamp = unpacked[8]
+                tx_timestamp = self.system_time_to_ntp(time.time())
+                response = pack('!B B b b I I 4s Q Q Q Q', 36, 1, 0, 0, 0, 0, b'LOCL', 0, recv_timestamp, tx_timestamp, tx_timestamp)
+                self.transport.write(response, addr)
+        except Exception as e:
+            log.msg(f"Error processing NTP request: {e}")
+
+def main():
+    parser = argparse.ArgumentParser(description='Run a simple NTP honeypot server.')
+    parser.add_argument('--host', type=str, default='0.0.0.0', help='Host interface to bind the NTP server to.')
+    parser.add_argument('--port', type=int, default=123, help='Port to bind the NTP server to.')
+    args = parser.parse_args()
+
+    LOG_FILE_PATH = os.path.join(script_dir, "ntp_honeypot.log")
+    print(f"NTP HONEYPOT ACTIVE ON HOST: {args.host}, PORT: {args.port}")
+    print(f"ALL attempts will be logged in: {LOG_FILE_PATH}")
+    log_observer = log.FileLogObserver(open(LOG_FILE_PATH, 'a'))
+    log.startLoggingWithObserver(log_observer.emit, setStdout=False)
+
+    reactor.listenUDP(args.port, SimpleNTPProtocol(), interface=args.host)
+    reactor.run()
+
+if __name__ == "__main__":
+    main()
